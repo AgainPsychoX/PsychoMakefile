afc44fa7197a0fc7cc766e831e24277b3e66d345
PsychoX <psychoxivi@gmail.com>
2017-12-28 20:52:20 +0100

    Uploaded version from SzczurEngine project

---
+++ settings.mk
@@ -0,0 +1,25 @@
+
+#
+# Settings
+#
+
+# Default target
+TARGET := win64
+
+# MXE config
+MXE := yes
+MXE_DIR := /mnt/e/Libraries/MXE/mxe
+
+# Fancy
+COLORS = yes
+
+# Lua sol2
+HEADER_INC_SOL2 := /mnt/e/Libraries/Lua-sol2-2.18.7/include
+# JSON
+HEADER_INC_JSON := /mnt/e/Libraries/JSON-2.1.1/include
+
+# DragonBones \w SFML wrapper
+SRC_DIRS += /mnt/e/Libraries/DragonBones/DragonBones/src /mnt/e/Libraries/DragonBones/SFML/src /mnt/e/Libraries/DragonBones/3rdParty
+INC_DIRS += /mnt/e/Libraries/DragonBones/DragonBones/src /mnt/e/Libraries/DragonBones/SFML/src /mnt/e/Libraries/DragonBones/3rdParty
+
+
