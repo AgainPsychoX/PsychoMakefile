c59f3c15bd4a0991743dbabae546825aebb4f6bc
PsychoX <psychoxivi@gmail.com>
2018-03-23 20:06:48 +0100

    Update settings.mk

--- settings.mk
+++ settings.mk
@@ -3,23 +3,59 @@
 # Settings
 #
 
-# Default target
-TARGET := win64
-
-# MXE config
-MXE := yes
-MXE_DIR := /mnt/e/Libraries/MXE/mxe
+# Compiler
+#CROSS := no # Uncomment 
+#CXX := clang
+#LD := clang
 
 # Fancy
 COLORS = yes
 
+
+
+# SFML 32bit
+INC_DIR_SFML_32 := e:/Libraries/SFML-2.4.2/include
+LIB_DIR_SFML_32 := e:/Libraries/SFML-2.4.2/MinGW-7.2-x86_64/lib
+BIN_DIR_SFML_32 := e:/Libraries/SFML-2.4.2/MinGW-7.2-x86_64/bin
+# SFML 64bit
+INC_DIR_SFML_64 := e:/Libraries/SFML-2.4.2/include
+LIB_DIR_SFML_64 := e:/Libraries/SFML-2.4.2/MinGW-7.2-x86_64/lib
+BIN_DIR_SFML_64 := e:/Libraries/SFML-2.4.2/MinGW-7.2-x86_64/bin
+
+# ImGUI 32bit
+INC_DIR_IMGUI_32 := /mnt/e/Libraries/ImGui-1.60/include
+LIB_DIR_IMGUI_32 := /mnt/e/Libraries/ImGui-1.60/lib/MinGW-7.2.0-x86_64
+BIN_DIR_IMGUI_32 := /mnt/e/Libraries/ImGui-1.60/lib/MinGW-7.2.0-x86_64
+# ImGUI 64bit
+INC_DIR_IMGUI_64 := /mnt/e/Libraries/ImGui-1.60/include
+LIB_DIR_IMGUI_64 := /mnt/e/Libraries/ImGui-1.60/lib/MinGW-7.2.0-x86_64
+BIN_DIR_IMGUI_64 := /mnt/e/Libraries/ImGui-1.60/lib/MinGW-7.2.0-x86_64
+
+# Boost 32bit
+INC_DIR_BOOST_32 := e:/Libraries/Boost-1.65.1/boost
+LIB_DIR_BOOST_32 := e:/Libraries/Boost-1.65.1/MinGW-7.2-x86_64/lib
+BIN_DIR_BOOST_32 := e:/Libraries/Boost-1.65.1/MinGW-7.2-x86_64/bin
+# Boost 64bit
+INC_DIR_BOOST_64 := e:/Libraries/Boost-1.65.1/boost
+LIB_DIR_BOOST_64 := e:/Libraries/Boost-1.65.1/MinGW-7.2-x86_64/lib
+BIN_DIR_BOOST_64 := e:/Libraries/Boost-1.65.1/MinGW-7.2-x86_64/bin
+
+# Lua 32bit
+INC_DIR_LUA_32 := e:/Libraries/Lua-5.3.4/src
+LIB_DIR_LUA_32 := e:/Libraries/Lua-5.3.4/MinGW-7.2-x86_64/lib
+BIN_DIR_LUA_32 := e:/Libraries/Lua-5.3.4/MinGW-7.2-x86_64/bin
+# Lua 64bit
+INC_DIR_LUA_64 := e:/Libraries/Lua-5.3.4/src
+LIB_DIR_LUA_64 := e:/Libraries/Lua-5.3.4/MinGW-7.2-x86_64/lib
+BIN_DIR_LUA_64 := e:/Libraries/Lua-5.3.4/MinGW-7.2-x86_64/bin
+
 # Lua sol2
-HEADER_INC_SOL2 := /mnt/e/Libraries/Lua-sol2-2.18.7/include
+HEADER_INC_SOL2 := e:/Libraries/Lua-sol2-2.18.7/include
 # JSON
-HEADER_INC_JSON := /mnt/e/Libraries/JSON-2.1.1/include
+HEADER_INC_JSON := e:/Libraries/JSON-2.1.1/include
 
 # DragonBones \w SFML wrapper
-SRC_DIRS += /mnt/e/Libraries/DragonBones/DragonBones/src /mnt/e/Libraries/DragonBones/SFML/src /mnt/e/Libraries/DragonBones/3rdParty
-INC_DIRS += /mnt/e/Libraries/DragonBones/DragonBones/src /mnt/e/Libraries/DragonBones/SFML/src /mnt/e/Libraries/DragonBones/3rdParty
+SRC_DIRS += e:/Libraries/DragonBones/DragonBones/src e:/Libraries/DragonBones/SFML/src e:/Libraries/DragonBones/3rdParty
+INC_DIRS += e:/Libraries/DragonBones/DragonBones/src e:/Libraries/DragonBones/SFML/src e:/Libraries/DragonBones/3rdParty
 
 
